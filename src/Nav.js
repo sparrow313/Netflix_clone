be8090e1638ae148aacee748816cd0c6f58b0d3a
sparrow313 <98008251+sparrow313@users.noreply.github.com>
2022-04-20 14:01:08 +0530

    final push

---
+++ src/Nav.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { useEffect, useState } from "react/cjs/react.development";
+import "./Nav.css";
+
+function Nav() {
+  const [show, handleShow] = useState();
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav_black"}`}>
+      <img
+        className="nav_logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+        alt="Netflix Logo"
+      />
+
+      <img
+        className="nav_avatar"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+        alt="Netflix avatar"
+      />
+    </div>
+  );
+}
+
+export default Nav;
---
+++ src/request.js
@@ -0,0 +1,14 @@
+const API_KEY = "ee068eb50806fb7d7c68d247d5af5ea6";
+
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRated: `/movie.top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorroeMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+};
+
+export default requests;
